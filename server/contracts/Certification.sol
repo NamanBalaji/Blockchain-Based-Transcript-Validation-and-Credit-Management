9cdd107b431524847ac126a1a33265d2c7e23e0f
namanQuick <namanb487@gmail.com>
2021-04-10 11:29:58 +0530

    Added routes

--- server/contracts/Certification.sol
+++ server/contracts/Certification.sol
@@ -1,7 +1,8 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Certification {
-    constructor() public {}
+    
 
     struct Certificate {
         string candidate_name;
--- server/migrations/2_deploy_contract.js
+++
@@ -1,5 +0,0 @@
-var Certification = artifacts.require("./Certification.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Certification);
-};
